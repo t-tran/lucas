978b170fa7435937214dab59defc38b7da024fd7
T. Tran <t-tran@users.noreply.github.com>
2018-01-16 17:15:22 +0700

    Add content-type

--- backend/handlers/cssHandler.go
+++ backend/handlers/cssHandler.go
@@ -7,6 +7,7 @@ import (
 )
 
 func CssHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/css")
     if _, err := os.Stat("static/index.css"); err == nil {
         data, err := ioutil.ReadFile("static/index.css")
         if (err == nil) {
--- backend/handlers/jsHandler.go
+++ backend/handlers/jsHandler.go
@@ -7,6 +7,7 @@ import (
 )
 
 func JsHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
     if _, err := os.Stat("static/index.js"); err == nil {
         data, err := ioutil.ReadFile("static/index.js")
         if (err == nil) {
